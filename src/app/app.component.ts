87b8e484a7cdc6b1654bd9c4bfb2a86dc6cd3d47
ekcs-swayam <ssuryavanshi@mediaferry.com>
2023-12-13 21:26:47 +0530

    forms basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,44 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'form';
+  submit(user: any, pass: any) {
+    let reg = /^[a-z]+$/i;
+    let flag = false;
+
+    if (user.value != '' && pass.value != '') {
+      
+      if(!reg.test(user.value)){
+        flag = true
+        alert("Username invalid")
+      }
+
+      if(pass.value.length < 6){
+        flag = true
+        alert("Password length must be greater than 6")
+      }
+
+
+      if(flag != true){
+        console.log(user.value);
+        console.log(pass.value);
+      }
+
+
+    } else {
+      alert('Username or Password cannot be empty');
+    }
+
+    return false;
+  }
+
+  showPassword(value: any, pass: any) {
+    if (value.checked) {
+      pass.type = 'text';
+    } else {
+      pass.type = 'password';
+    }
+  }
 }
